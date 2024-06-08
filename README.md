4d3ea12e342886147064f666400fe4a91bd14523
nullcharge <nullagent07@gmail.com>
2024-06-08 15:04:21 +0200

    Added image preprocessing and OCR functionality using OpenCV and pytesseract

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# memes-bot
+# memes-bot   
+```python 
+    # Creating a virtual environment
+    python -m venv myenv
+
+    # Activating the virtual environment
+    source myenv/bin/activate  # Unix или MacOS
+
+    # Installing the necessary libraries
+    pip install opencv-python-headless
+```
+
---
+++ main.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+from PIL import Image
+import pytesseract
+
+# Uploading an image
+image_path = './images/image.png'
+image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+# Application of median filter for noise removal
+image = cv2.medianBlur(image, 3)
+
+# Image binarisation
+_, binary_image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+# Saving a pre-processed image for inspection
+preprocessed_image_path = 'preprocessed_image.png'
+cv2.imwrite(preprocessed_image_path, binary_image)
+
+# Performing OCR for text extraction
+image_pil = Image.fromarray(binary_image)
+text = pytesseract.image_to_string(image_pil, lang='eng')
+
+print("Recognised text:")
+print(text)
